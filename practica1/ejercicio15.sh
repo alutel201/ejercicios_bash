526b61f8432f8319e7142f7f9e5f1a7643d25bfb
Jaume <alutel201@ieselcaminas.org>
2023-12-21 09:08:24 +0100

    ejercicioscorrejidos

--- practica1/ejercicio15.sh
+++ practica1/ejercicio15.sh
@@ -10,23 +10,29 @@ if [ ! -d "$directorio" ]; then
     echo "Error: El directorio '$directorio' no existe."
     exit 1
 fi
-
+numero_entradas=0
 echo "Listado de entradas en el directorio '$directorio':"
 for entrada in "$directorio"/*; do
+    nombre=$(basename "$entrada")
     if [ -f "$entrada" ]; then
-        echo "$entrada es un fichero regular."
+        echo "$nombre es un fichero regular."
+        ((numero_entradas++))
     elif [ -d "$entrada" ]; then
-        echo "$entrada es un directorio."
+        echo "$nombre es un directorio."
+        ((numero_entradas++))
     elif [ -L "$entrada" ]; then
-        echo "$entrada es un enlace simbolico."
+        echo "$nombre es un enlace simbolico."
+        ((numero_entradas++))
     elif [ -b "$entrada" ]; then
-        echo "$entrada es un archivo especial de bloque."
+        echo "$nombre es un archivo especial de bloque."
+        ((numero_entradas++))
     elif [ -c "$entrada" ]; then
-        echo "$entrada es un archivo especial de caracteres."
+        echo "$nombre es un archivo especial de caracteres."
+        ((numero_entradas++))
     else
-        echo "$entrada no se reconoce como un fichero, directorio, enlace simbólico, archivo especial de bloque ni archivo especial de caracteres."
+        echo "$nombre no se reconoce como un fichero, directorio, enlace simbolico, archivo especial de bloque ni archivo especial de caracteres."
+        ((numero_entradas++))
     fi
 done
 
-numero_entradas=$(find "$directorio" -maxdepth 1 -type f -o -type d -o -type l -o -type b -o -type c | wc -l)
 echo "Numero total de entradas procesadas: $numero_entradas"
