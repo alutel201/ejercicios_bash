56f7feb288a9a0ae02deee735b375f6e6e2befe6
alutel201 <79305373+alutel201@users.noreply.github.com>
2024-01-08 19:25:00 +0100

    ejercicio1.sh

--- practica4/ejercicio1.sh
+++ practica4/ejercicio1.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-last_output=$(last)
-
-ip_lines=$(echo "$last_output" | egrep -o '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b')
-
-echo "Usuarios que iniciaron sesion con direcciones IP:"
-echo "$ip_lines"
-
+echo "Reiniciando en 5 minutos."
+sleep 300
+sudo reboot
